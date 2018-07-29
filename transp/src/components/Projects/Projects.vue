a6fe725cedccda16a940beb812ff9fae188e94e0
Adnan <ayunus@ucsc.edu>
2018-07-29 15:28:46 -0700

    Payment Array set up

--- transp/src/components/Projects/Projects.vue
+++ transp/src/components/Projects/Projects.vue
@@ -7,11 +7,20 @@
           <div id="fundsInfo" style="display:inline-block;">${{project.current_fund}} out of ${{project.fund_goal}} raised
           <progress style="display:inline-block;float:right;margin-right:150px;" id="file" name="file" :max="project.fund_goal" :value="project.current_fund">
           </progress>
-          <router-link to="/projectDetail" style="color: black"><a class="button3">Donate</a></router-link>
+          <button class = "button3" @click = "project_button(project.projectID)">Donate</button>
         </div>
         </div>
         <div id="fundsInfo" style="font-weight:400i">{{project.days_left}} days remaining</div>
+
+
       </div>
+      <div v-if = "is_viewing_funds" style = "display:block;border-radius:12px;width:70%;margin-right:7%;padding-top:1%;padding-left:1%;padding-right:1%;margin-top:2px;margin-left:22%;height:150px;background-color:#363E45;">
+        <div class = "listview" style = "height:100px;overflow-y:scroll;">
+          <div v-for = "row in filteredFundArr" style = "position:relative;border-top:1px solid grey;height:55px"><div style = "position:absolute;margin-top:16px;margin-left:5px;display:inline-block;color:white;">{{row.paymentPerson}} payed {{row.payed}}</div>
+          </div>
+        </div>
+      </div>
+
   </div>
 </template>
 
@@ -60,6 +69,7 @@ label {
   margin-left:28%;
   margin-top:15px;
   padding:10px;
+  display:block;
 }
 
 #projectTitle{
@@ -105,12 +115,52 @@ export default {
         {title: "Repaint The Walls", fund_goal:300, current_fund:200, thumbnail: "https://s8.postimg.cc/z1q9nwb85/wall1.jpg", days_left:8, projectID:"walls", mosqueID:"yaseen"},
         {title:"Fix the carpet", fund_goal:900, current_fund:750, thumbnail:"https://s8.postimg.cc/fvd2ktpmd/carpet1.jpg", days_left:19, projectID:"carpet", mosqueID:"yaseen"}
       ],
+      funds: [{
+        "id":"walls",
+        "paymentPerson": "Egan",
+        "payed": "150"
+      },
+      {
+        "id":"carpet",
+        "paymentPerson": "Mohammed",
+        "payed": "550"
+      },
+      {
+        "id":"walls",
+        "paymentPerson": "Anonymous",
+        "payed": "50"
+      },
+      {
+        "id":"carpet",
+        "paymentPerson": "Ulasyar",
+        "payed": "80"
+      },
+      {
+        "id":"carpet",
+        "paymentPerson": "Anonymous",
+        "payed": "120"
+      }
+      ],
+      filteredFundArr: [],
+      is_viewing_funds: false
     }
   },
   methods: {
     message: function(i) {
       console.log(i);
-    }
+    },
+    project_button: function (id) {
+      this.filteredFundArr = [];
+       // The button to edit a memo has been pressed.
+       for(var i = 0;i<this.funds.length;i++) {
+         if((this.funds[i].id) == id) {
+           this.filteredFundArr.push(this.funds[i]);
+         }
+       }
+       this.is_viewing_funds = true;
+       console.log(this.filteredFundArr);
+     }
+
   }
 }
 </script>
